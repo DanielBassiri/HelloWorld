9435a834a823f79d434dbde27f14c5d50226b7c2
DanielBassiri <daniel-bassiri@hotmail.co.uk>
2017-06-03 20:54:45 +0100

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Daniel on 03/06/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println("Hello");
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Daniel on 03/06/2017.
+ */
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        int result = helloWorld.multiply(20, 10);
+        Assert.assertEquals(result, 200);
+    }
+}
